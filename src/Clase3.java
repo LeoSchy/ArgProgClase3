60982ac3f7be034b3716210842976528e0b60674
LeoSchy <schymansky.leonardo@gmail.com>
2023-03-19 20:16:03 -0300

    Commit de la clase 3, punto a y b listos.

--- src/Clase3.java
+++ src/Clase3.java
@@ -1,8 +1,8 @@
 import java.lang.String;
 public class Clase3 {
     public static void main(String[] args) {
-        class punto_a {
-            public static int cuentaLetra(String cadenaEntrada, char letra) {
+        class ejercicio_1 {
+            public static int punto_a(String cadenaEntrada, char letra) {
                 int apariciones = 0;
                 for (int i = 0; i < cadenaEntrada.length(); i++) {
                     if (cadenaEntrada.charAt(i) == letra) {
@@ -12,16 +12,34 @@ public class Clase3 {
                 return apariciones;
             }
 
-            static String cadenaEjemplo = new String("Esta es una cadena de ejemplo");
-            static char letraEjemplo = 'e';
-            static int apariciones = cuentaLetra(cadenaEjemplo, letraEjemplo);
+            public static int[] punto_b(int num1, int num2, int num3, boolean esDescendiente) {
+                    int[] nums = { num1, num2, num3 };
+                    if (esDescendiente) {
+                        for (int i = 0; i < nums.length - 1; i++) {
+                            for (int j = i + 1; j < nums.length; j++) {
+                                if (nums[i] < nums[j]) {
+                                    int temp = nums[i];
+                                    nums[i] = nums[j];
+                                    nums[j] = temp;
+                                }
+                            }
+                        }
+                    } else {
+                        for (int i = 0; i < nums.length - 1; i++) {
+                            for (int j = i + 1; j < nums.length; j++) {
+                                if (nums[i] > nums[j]) {
+                                    int temp = nums[i];
+                                    nums[i] = nums[j];
+                                    nums[j] = temp;
+                                }
+                            }
+                        }
+                    }
+                    return nums;
+                }
+            /** punto_c **/
+
         }
-
-        class punto_b {
-
-        }
-
-        System.out.println("La letra '" + punto_a.letraEjemplo + "' aparece: " + punto_a.apariciones + " veces.");
 
     }
 }
