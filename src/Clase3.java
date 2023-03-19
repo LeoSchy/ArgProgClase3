143372b7bd78df095fd86cb980666d6ffaed831c
LeoSchy <schymansky.leonardo@gmail.com>
2023-03-19 19:11:06 -0300

    Commit de la clase 3, punto a listo.

--- src/Clase3.java
+++ src/Clase3.java
@@ -1,5 +1,27 @@
+import java.lang.String;
 public class Clase3 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        class punto_a {
+            public static int cuentaLetra(String cadenaEntrada, char letra) {
+                int apariciones = 0;
+                for (int i = 0; i < cadenaEntrada.length(); i++) {
+                    if (cadenaEntrada.charAt(i) == letra) {
+                        apariciones++;
+                    }
+                }
+                return apariciones;
+            }
+
+            static String cadenaEjemplo = new String("Esta es una cadena de ejemplo");
+            static char letraEjemplo = 'e';
+            static int apariciones = cuentaLetra(cadenaEjemplo, letraEjemplo);
+        }
+
+        class punto_b {
+
+        }
+
+        System.out.println("La letra '" + punto_a.letraEjemplo + "' aparece: " + punto_a.apariciones + " veces.");
+
     }
 }
